836b56eb577e0c5b22957d98c05f4ab9cbcf4a52
flight28agency <flight28agency@gmail.com>
2026-08-22 02:01:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { createClient } = require("@supabase/supabase-js");
-const { WebcastPushConnection } = require("tiktok-live-connector");
-
+const { TikTokLiveConnection } = require("tiktok-live-connector");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
