928d4652bc010a505836a29799945808db78b5a0
flight28agency <flight28agency@gmail.com>
2026-08-22 20:28:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -131,8 +131,6 @@ if (diamonds > 0) {
   });
 }
 
-} catch (err) {
-  console.log(`Could not parse event for @${username}:`, err.message);
 }
   ws.on("error", err => {
     console.log(`WebSocket error for @${username}:`, err.message);
